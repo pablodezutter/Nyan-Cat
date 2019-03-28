49a696d828739bc94bcab994dc4080c7b23970fd
p <dezutterpablo@gmail.com>
2019-03-28 13:31:28 +0100

    trying to get the good code

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 //Als er op de button geklikt wordt, komt er een img van Nyancat in de body
-console.log
+
 //wat is de button
 var button = document.getElementById(button);
 
@@ -8,12 +8,16 @@ var button = document.getElementById(button);
  var nyancat = document.createElement("img");
 
 //welke handeling (click)
- document.getElementById('button')
-  .addEventListener ('click', mouseClick);
+
+  button.addEventListener ('click', mouseClick);
 
 function mouseClick(){
-  var button = document.getElementById(button);
-  document.createElement("img");
-  nyancat="nyan_cat.png";
-  nyancat="cat";
-}
+  if (document.getElementById("button")) {
+    return;
+
+  }
+  var element = document.createElement("img");
+  nyancat.src="nyan_cat.png";
+  nyancat.id="cat";
+
+  }
