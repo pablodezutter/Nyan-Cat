6f8c3682c57da45a813ea4547b6ba2e2d9e5ee5d
p <dezutterpablo@gmail.com>
2019-03-28 15:12:57 +0100

    after a long search, the cat appears!

--- myscript.js
+++ myscript.js
@@ -1,22 +1,22 @@
 //Als er op de button geklikt wordt, komt er een img van Nyancat in de body
 
 //wat is de button
-var button = document.getElementById(button);
+var button = document.getElementById("btn-cat");
 
 //wat is de img
 
- var nyancat = document.createElement("img");
-
+ var nyancat = document.createElement('img');
+console.log (nyancat);
 //welke handeling (click)
 
-  button.addEventListener ('click', mouseClick);
+button.addEventListener ('click', mouseClick);
 
 function mouseClick(){
-  if (document.getElementById("button")) {
-    return;
+  if (document.getElementById("btn-cat")) {
+
 
   }
-  var element = document.createElement("img");
+  var element = document.body.appendChild (nyancat);
   nyancat.src="nyan_cat.png";
   nyancat.id="cat";
 
