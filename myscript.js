8996fddc51c401315de5cacb2949c21e2aba38c9
p <dezutterpablo@gmail.com>
2019-03-28 17:01:11 +0100

    div toevoegen

--- myscript.js
+++ myscript.js
@@ -12,12 +12,27 @@ console.log (nyancat);
 button.addEventListener ('click', mouseClick);
 
 function mouseClick(){
-  if (document.getElementById("btn-cat")) {
-
-
-  }
+  // if (document.getElementById("btn-cat")) {
+  //
+  // }
   var element = document.body.appendChild (nyancat);
   nyancat.src="nyan_cat.png";
   nyancat.id="cat";
+  nyancat.style.width="100px";
+  nyancat.style.height="100px";
+}
 
-  }
+//Animation:
+
+// function myMove() {
+//   var elem = document.getElementById("cat");
+//   var pos = 0;
+//   var id = setInterval(frame, 5);
+//   function frame() {
+//     if (pos == 350) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos + "px";
+//       elem.style.left = pos + "px";
+//     }
